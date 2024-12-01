10eb66e1ec9b78a628e33a6cacafe160aae7d0fb
Ashutosh-Panda2004 <ashutosh2004.panda@gmail.com>
2024-12-01 22:54:04 +0530

    Removed Class files

---
+++ BFS_traversal_graph.java
@@ -0,0 +1,15 @@
+/*
+In both trees and graphs, BFS traversal explores nodes level by level, starting from the source (root in trees, starting node in graphs).
+
+- In trees, BFS (level-order) visits all nodes at the current level before moving to the next level.
+
+- In graphs, BFS explores all neighboring nodes of the current node before moving to the next level of neighbors.
+
+In both cases, a queue is used to manage the nodes to be visited.
+
+*/
+
+public class BFS_traversal_graph {
+
+    
+}
